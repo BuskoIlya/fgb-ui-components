bc8c9b72e05483436a3e18802a15875a2f3049a1
Busko Ilya <trassae95st@mail.ru>
2023-03-18 10:22:33 +0300

    Create CardTitle

---
+++ src/components/CardTitle/CardTitle.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { SmartComponent, SmartComponentProps } from "../SmartComponent";
+import './CardTitle.css';
+
+export interface CardTitleProps
+  extends Omit<SmartComponentProps, 'active' | 'className'>
+{
+  children?: string,
+  to: string
+}
+
+export const CardTitle = ({ children, color, Tag, ...props }: CardTitleProps) => {
+
+  return (
+    <SmartComponent color={color} className='CardTitle' Tag={Tag} {...props}>
+      {children}
+    </SmartComponent>
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 export { Button } from './components/Button';
 export type { ButtonProps } from './components/Button';
 
+export { CardTitle } from './components/CardTitle';
+export type { CardTitleProps } from './components/CardTitle';
+
+export { LinkDownload } from './components/Links';
+export type { LinkDownloadProps } from './components/Links';
+
+export { LinkExternal } from './components/Links';
+export type { LinkExternalProps } from './components/Links';
+
+export { LinkInner } from './components/Links';
+export type { LinkInnerProps } from './components/Links';
+
 export { TitleCountry } from './components/TitleCountry';
 export type { TitleCountryProps } from './components/TitleCountry';
